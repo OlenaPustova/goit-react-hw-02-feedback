a8e9e3e8adbbbd58f3b31641bf89e43201a82201
OlenaPustova <i.am.elena.p@gmail.com>
2022-05-04 19:18:55 +0300

    homework feedback

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+// import { Component } from 'react';
+import Statistics from 'components/Statistics/Statistics';
+import FeedbackOptions from 'components/FeedbackOptions/FeedbackOptions';
+import Section from 'components/Section/Section';
+import Notification from 'components/Notification/Notification';
+import { Component } from 'react';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  changePoint = e => {
+    this.setState(prevState => {
+      const currentName = e.target.name;
+      return { [currentName]: prevState[currentName] + 1 };
+    });
+  };
+
+  countTotalFeedback = () =>
+    this.state.good + this.state.neutral + this.state.bad;
+
+  countPositiveFeedbackPercentage = () => {
+    const total = this.countTotalFeedback();
+    return Math.round((this.state.good * 100) / total);
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const totalFeedback = this.countTotalFeedback();
+    return (
+      <>
+        <Section title="Please leave feedback">
+          <FeedbackOptions onPoint={0} onLeaveFeedback={this.changePoint} />
+        </Section>
+
+        <Section title="Statistics">
+          {!!totalFeedback && (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={totalFeedback}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          )}
+        </Section>
+
+        {!totalFeedback && <Notification message="There is no feedback" />}
+      </>
+    );
+  }
+}
+
+export default App;
